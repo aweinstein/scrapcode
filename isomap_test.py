256cc674a9bc9ba1452cdb54bdcff49ead1bcc9c
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2011-11-06 19:21:36 -0700

    Updated the print statements.

--- isomap_test.py
+++ isomap_test.py
@@ -9,16 +9,15 @@ out_dim = 3
 
 print 'Using random data'
 Y = manifold.Isomap(n_neighbors, out_dim).fit_transform(X)
-print 'out_dim:%d Y shape: %s' % (out_dim, Y.shape)
+print 'X shape: %s, out_dim:%d Y shape: %s' % (X.shape, out_dim, Y.shape)
 
 print 'Using the data X.npy and Isomap'
 X = np.load('X.npy')
 Y = manifold.Isomap(n_neighbors, out_dim).fit_transform(X)
-print 'out_dim:%d Y shape: %s' % (out_dim, Y.shape)
+print 'X shape: %s, out_dim:%d Y shape: %s' % (X.shape, out_dim, Y.shape)
 
 
 Y = manifold.LocallyLinearEmbedding(n_neighbors, out_dim).fit_transform(X)
 
 print 'Using the data X.npy and LLE'
-print 'out_dim:%d Y shape: %s' % (out_dim, Y.shape)
-
+print 'X shape: %s, out_dim:%d Y shape: %s' % (X.shape, out_dim, Y.shape)
