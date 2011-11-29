0d38ae16a01cc7cbbf0354415b3a375c0905867d
aweinstein <alejandr.weinstein@gmail.com>
2011-11-29 08:39:15 -0700

    Added left_ev.py.

---
+++ np/left_ev.py
@@ -0,0 +1,43 @@
+"""Computing the left eigenvector of a non-negative matrix"""
+
+import numpy as np
+
+A = np.array([[0.8147, 0.0975, 0.1576, 0.1419, 0.6557],
+              [0.9058, 0.2785, 0, 0.4218, 0.0357],
+              [0.1270, 0.5469, 0.9572, 0.9157, 0.8491],
+              [0, 0.9575, 0.4854, 0.7922, 0.9340],
+              [0.6324, 0.9649, 0.8003, 0.9595, 0.6787]])
+evals, evecs = np.linalg.eig(A.T)
+rho = evals.max()
+phi = evecs[:, evals==rho]
+phi /= phi.sum()  # Normalize to unit sum, so it is the Perron vector
+
+# Sanity check
+print np.dot(phi.T, A)
+print rho * phi.T
+
+
+
+
+
+
+
+
+
+
+
+
+
+## A = [
+##     0.8147    0.0975    0.1576    0.1419    0.6557;
+##     0.9058    0.2785         0    0.4218    0.0357;
+##     0.1270    0.5469    0.9572    0.9157    0.8491;
+##          0    0.9575    0.4854    0.7922    0.9340;
+##     0.6324    0.9649    0.8003    0.9595    0.6787;];
+
+## [evec, eval] = eig(A');
+## rho = max(eval(:));
+## phi = evec(:, eval==rho);
+## phi = phi / sum(phi);
+## phi' * A
+## rho * phi
