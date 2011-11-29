fa64f13dedd18b548c6cb3c113746d1062f09e27
aweinstein <alejandr.weinstein@gmail.com>
2011-11-29 08:42:28 -0700

    Removed reference to Matlab code.

--- np/left_ev.py
+++ np/left_ev.py
@@ -1,4 +1,6 @@
-"""Computing the left eigenvector of a non-negative matrix"""
+"""Computing the left eigenvector of a non-negative matrix. We find
+the left eigenvector corresponding to the biggest eigenvalue, i.e.,
+the Perron vector, and normalize so it has unit sum."""
 
 import numpy as np
 
@@ -15,29 +17,3 @@ phi /= phi.sum()  # Normalize to unit sum, so it is the Perron vector
 # Sanity check
 print np.dot(phi.T, A)
 print rho * phi.T
-
-
-
-
-
-
-
-
-
-
-
-
-
-## A = [
-##     0.8147    0.0975    0.1576    0.1419    0.6557;
-##     0.9058    0.2785         0    0.4218    0.0357;
-##     0.1270    0.5469    0.9572    0.9157    0.8491;
-##          0    0.9575    0.4854    0.7922    0.9340;
-##     0.6324    0.9649    0.8003    0.9595    0.6787;];
-
-## [evec, eval] = eig(A');
-## rho = max(eval(:));
-## phi = evec(:, eval==rho);
-## phi = phi / sum(phi);
-## phi' * A
-## rho * phi
