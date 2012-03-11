d8609509a63e78afc3b74373d4d5bb8d14a307f6
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2012-03-10 21:00:42 -0700

    Added tutorials 12, 13, and 14.

---
+++ java_tutorials/chute.java
@@ -0,0 +1,19 @@
+class chute
+{
+    chute()  
+    {
+        System.out.println("I'm chute.");
+        cute cu = new cute();
+    }
+}
+
+class cute 
+{
+    cute() 
+    {
+        System.out.println("I'm cute.");
+    }
+}
+
+
+        
---
+++ java_tutorials/pacman/maze/labyrinth.java
@@ -0,0 +1,10 @@
+package pacman.maze;
+
+public class labyrinth
+{
+    public labyrinth()
+    {
+        System.out.println("I trap minotaur!");
+        
+    }
+}
