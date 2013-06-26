fcb0e41f8f78df74ab36f0951cd5b951ff464140
Alejandro Weinstein <alejandro.weinstein@gmail.com>
2013-06-26 09:44:13 -0400

    Update latex_alg_trans.py.

--- others/latex_alg_trans.py
+++ others/latex_alg_trans.py
@@ -1,39 +1,30 @@
 
-in_txt =r'''\REQUIRE  $A$, $b$, $J$, stopping criterion
-\STATE \textbf{initialize:} $r^{(0)} = b$, $\Gamma^{(0)} =
-  \emptyset$, $\ell = 0$
+in_txt = r'''\REQUIRE  $A$, $y$, stopping criterion
+\STATE $x_0 =0$
+\STATE $c_0 = A^Ty$
+\STATE $\lambda_0, I_0 = \max_i(|c_0(i)|)$
+\STATE $k = 0$
 \WHILE{not converged}
-\STATE
-\begin{tabular}{ll}
-  \textbf{match:} & $h(j) = \vnorm{A_j^T r}$, $j = 1, \ldots, J$ \\
-  \textbf{identify:} & $\Gamma^{(\ell+1)} =
-  \Gamma^{(\ell)} \cup \argmax_{j} h(j)$ \\
-  \textbf{update:} & $x^{\ell+1} = \argmin_{z: ~ \supp(z) \subseteq
-     \Gamma^{(\ell+1)}}  \|b - A z\|_2$ \\
-   & $r^{\ell+1} = b - A x^{\ell + 1}$ \\
-   & $\ell = \ell+1$ \\
-\end{tabular}
+\STATE $c_k = c_0 - A^T A x$
+\STATE $d_k = \vc{0}$
+\STATE $d_k(I_k) = \left(A_{I_k}^T A_{I_k})^{-1} \sign(c_k(I_k) \right)$
+\STATE $\gamma_k^+, i_k^+ = \min_{i \in I^c}^+ \left\{
+  \frac{\lambda_k - c_k(i)}{1 - a_i^TA_Id_k(I)},
+  \frac{\lambda_k + c_k(i)}{1 + a_i^TA_Id_k(I)}
+  \right\}$
+\STATE $\gamma_k^-, i_k^- = \min_{i \in I}^+\left\{ -x_k(i) / d_k(i) \right\}$
+\IF{$\gamma_k^+ < \gamma_k^-$}
+\STATE $\gamma_k = \gamma_k^+$
+\STATE $I_{k+1} = I_k \cup \{i_k^+\}$
+\ELSE
+\STATE $\gamma_k = \gamma_k^-$
+\STATE $I_{k+1} = I_k \backslash \{i_k^-\}$
+\ENDIF
+\STATE $x_{k+1} = x_k + \gamma_k d_k$
+\STATE $\lambda_{k+1} = \lambda_k - \gamma_k$
+\STATE $k = k + 1$
 \ENDWHILE
-\ENSURE $\widehat{x} = x^{\ell} = \argmin_{z: ~ \supp(z) \subseteq
-  \Gamma^{(\ell)}} \|b - A z\|_2$'''
-
-in_txt = r'''\REQUIRE  $A$, $b$, stopping criterion
-\State \textbf{initialize:} $r^{(0)} = b$, $\Gamma^{(0)} =
-  \emptyset$, $\ell = 0$
-\WHILE{not converged}
-\STATE
-\begin{tabular}{ll}
-  \textbf{match:} & $h = |A^T r |$ \\
-  \textbf{identify:} & $\Gamma^{(\ell+1)} =
-  \Gamma^{(\ell)} \cup \argmax_{j} |h(j)|$ \\
-  \textbf{update:} & $x^{\ell+1} = \argmin_{z: ~ \supp(z) \subseteq
-     \Gamma^{(\ell+1)}}  \|b - A z\|_2$ \\
-   & $r^{\ell+1} = b - A x^{\ell + 1}$ \\
-   & $\ell = \ell+1$ \\
-\end{tabular}
-\ENDWHILE
-\ENSURE $\widehat{x} = x^{\ell} = \argmin_{z: ~ \supp(z) \subseteq
-  \Gamma^{(\ell)}} \|b - A z\|_2$'''
+\ENSURE $x_j, \lambda_j$ for $j = 1, \ldots, k$'''
 
 r_list = {'REQUIRE': 'Require',
           'ENSURE': 'Ensure',
@@ -41,7 +32,15 @@ r_list = {'REQUIRE': 'Require',
           'WHILE': 'While',
           'ENDWHILE': 'EndWhile',
           'IF': 'If',
-          'ENDIF': 'EndIf'}
+          'ENDIF': 'EndIf',
+          'ELSE': 'Else',
+          'REPEAT': 'Repeat',
+          'UNTIL': 'Until',
+          'FOR': 'For',
+          'ENDFOR': 'EndFor',
+          'END': 'End',
+          '\OR': 'OR',
+          }
 
 out_txt = in_txt
 for k,v in r_list.iteritems():
